e2a711b5299aeb454dba8fee88e8377b5738e241
Kierra <kierra1295@gmail.com>
2018-03-26 21:23:00 -0400

    - Add the name column to the users table. - Include name attribute to sign_up_params and account_update_params   methods inside the RegistrationsController

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'pages#index'
 
-  devise_scope :user do
+  devise_for :users, :controllers => {:registrations => "registrations"} do
     get 'login', to: 'devise/sessions#new'
   end
 end
